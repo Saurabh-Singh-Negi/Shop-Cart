9894da1254a3d6632471e9d30abc7901c865b7a6
saurabh <saurabhnegi0007@gmail.com>
2024-05-21 00:38:44 +0530

    cartitem modal ui and remove functionality

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -1,24 +1,40 @@
-import { Stack } from "rsuite";
+import { Text } from "rsuite";
 import storeData from "../../data/items.json";
-
+import styles from "./CartItem.module.css";
+import { useShoppingCart } from "../../context/ShoppingCartContext";
 type CartItemProps = {
   id: number;
   quantity: number;
 };
 
 const CartItem = ({ id, quantity }: CartItemProps) => {
-  console.log("item", id);
-  console.log("item", quantity);
+  const { removeQuantity } = useShoppingCart();
   const storeItem = storeData.find((items) => items.id === id);
+  const totalPrice = storeItem ? storeItem.price * quantity : 0;
   return (
-    <Stack>
-      <div>
-        <img width={100} height={100} src={storeItem?.imgUrl} alt="" />
+    <>
+      <div className={styles.mainContainer}>
+        <div>
+          <img width={100} height={60} src={storeItem?.imgUrl} alt="" />
+        </div>
+        <div className={styles.subContainer}>
+          <div>
+            <Text>{storeItem?.name}</Text>
+            <Text>{storeItem?.price}</Text>
+          </div>
+
+          <div className={styles.removeContainer}>
+            <span>{totalPrice}</span>
+            <button
+              onClick={() => removeQuantity(id)}
+              className={styles.removeBtn}
+            >
+              x
+            </button>
+          </div>
+        </div>
       </div>
-      <Stack>
-        <p>{storeItem?.name}</p>
-      </Stack>
-    </Stack>
+    </>
   );
 };
 
