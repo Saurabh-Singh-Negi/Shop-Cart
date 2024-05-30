4257ed5f9283492ed81e6c4e9f42b0c286a0aeec
saurabh <saurabhnegi458@gmail.com>
2024-05-30 23:36:50 +0530

    removed 007

--- src/components/modal/CartModal.tsx
+++ src/components/modal/CartModal.tsx
@@ -20,7 +20,6 @@ const CartModal = () => {
           Cart
         </p>
         <p>New 458</p>
-        <p>new line from 007</p>
         <div style={{ paddingTop: "2rem" }}>
           {cartItems.map((item) => (
             <CartItem key={item.id} {...item} />
