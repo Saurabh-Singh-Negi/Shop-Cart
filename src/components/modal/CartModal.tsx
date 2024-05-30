20adc4063555463097efc2eec980f0261042fce8
saurabh <saurabhnegi458@gmail.com>
2024-05-30 23:28:06 +0530

    new line 007

--- src/components/modal/CartModal.tsx
+++ src/components/modal/CartModal.tsx
@@ -18,6 +18,7 @@ const CartModal = () => {
         >
           Cart
         </p>
+        <p>new line from 007</p>
         <div style={{ paddingTop: "2rem" }}>
           {cartItems.map((item) => (
             <CartItem key={item.id} {...item} />
