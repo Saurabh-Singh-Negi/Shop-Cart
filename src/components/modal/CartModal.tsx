9f0603807864326bae18004237c627d53a344de9
saurabh <saurabhnegi458@gmail.com>
2024-05-30 23:45:33 +0530

    new changes 007

--- src/components/modal/CartModal.tsx
+++ src/components/modal/CartModal.tsx
@@ -20,6 +20,7 @@ const CartModal = () => {
           Cart
         </p>
         <p>New 458</p>
+        <p>check this 007</p>
         <div style={{ paddingTop: "2rem" }}>
           {cartItems.map((item) => (
             <CartItem key={item.id} {...item} />
